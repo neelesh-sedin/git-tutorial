a654ac3d48ebd7dfafa48743397db9874ace88e7
Neelesh <neelesh@sedintechnologies.com>
2023-07-07 14:30:35 +0530

    commit message

--- git-commands.sh
+++ git-commands.sh
@@ -1,3 +1,13 @@
 git init
 git add git.txt
 git commit -m "First commit"
+
+git branch -M main
+git remote add origin https://github.com/neelesh-sedin/git-tutorial.git
+git push -u origin main
+
+git add .
+git commit -m "commit message"
+git push
+
+git pull
